fa2f3c148f7da053873bce10ff27f49824e979fd
Roy_Kao <roy_kao@win-way.com.tw>
2026-06-16 14:46:16 +0800

    test

--- topics/transaction-monitor/src/main/java/com/payment/TransactionMonitorApplication.java
+++ topics/transaction-monitor/src/main/java/com/payment/TransactionMonitorApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class TransactionMonitorApplication {
+    // test
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionMonitorApplication.class, args);
